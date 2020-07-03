ba3fcf48489547a762df4e509358761374011bd2
Tomas Boda <tomasboda77@gmail.com>
2020-07-03 12:57:26 +0200

    fav

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
+import Head from 'next/head';
 import '../styles/styles.css';
 import 'aos/dist/aos.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
