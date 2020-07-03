54c184fe680faa089ac2abdfb6f04e25391fc496
Tomas Boda <tomasboda77@gmail.com>
2020-07-03 12:47:30 +0200

    title and fixes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Home from '../components/Home/Home';
 import Layout from '../components/Layout/Layout';
 import Projects from '../components/Projects/Projects';
@@ -8,6 +9,10 @@ import Footer from '../components/Footer/Footer';
 function App() {
   return (
     <Layout>
+      <Head>
+        <title>Tomas Boda</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Home />
       <Projects />
       <Art />
